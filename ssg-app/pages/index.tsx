329e1e9cf35d80268dea66f25a7f59904d380861
Pavani <pavanisripamu@gmail.com>
2026-06-05 12:09:18 +0530

    fix(ssg): redirect root route to /products via next.config

--- ssg-app/pages/index.tsx
+++ ssg-app/pages/index.tsx
@@ -1,14 +1,3 @@
-import { GetStaticProps } from "next";
-
-export const getStaticProps: GetStaticProps = async () => {
-  return {
-    redirect: {
-      destination: "/products",
-      permanent: false,
-    },
-  };
-};
-
 export default function Home() {
   return null;
 }
